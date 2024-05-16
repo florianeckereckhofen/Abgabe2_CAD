0c3da51453a2ff43b4bb58128e305ecc91e902c9
Florian Ecker-Eckhofen <76865644+florianeckereckhofen@users.noreply.github.com>
2024-05-16 20:18:03 +0200

    Abgabe2_Fertig

---
+++ appSettings.js
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// <SettingsSnippet>
+const settings = {
+  'clientId': '5d09a74f-45fa-46a4-9048-9368b8c5f5f0',
+  'tenantId': 'common',
+  'graphUserScopes': [
+    'user.read',
+    'mail.read',
+    'mail.send',
+    'Calendars.Read',
+    'Calendars.ReadWrite'
+  ]
+};
+
+module.exports = settings;
+// </SettingsSnippet>
